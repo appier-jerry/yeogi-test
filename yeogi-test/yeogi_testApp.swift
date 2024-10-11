625016e37212d8753c2b1b5ae744af064af908c3
zero-hacker <kjhmodis@gmail.com>
2024-10-11 10:28:28 +0800

    Initial Commit

---
+++ yeogi-test/yeogi_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  yeogi_testApp.swift
+//  yeogi-test
+//
+//  Created by Jerry Kim on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct yeogi_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
